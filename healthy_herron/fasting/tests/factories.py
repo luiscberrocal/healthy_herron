f2bf0c3ca2962d680885785978dbf9568f0ba491
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2025-11-09 07:49:04 -0500

    Tests running in Pycharm.

--- healthy_herron/fasting/tests/factories.py
+++ healthy_herron/fasting/tests/factories.py
@@ -1,44 +1,29 @@
 import factory
-from django.contrib.auth import get_user_model
 from django.utils import timezone
 
 from healthy_herron.fasting.models import Fast
-
-User = get_user_model()
-
-
-class UserFactory(factory.django.DjangoModelFactory):
-    """Factory for creating User instances for testing."""
-    
-    class Meta:
-        model = User
-        django_get_or_create = ('username',)
-    
-    username = factory.Sequence(lambda n: f"user{n}")
-    email = factory.LazyAttribute(lambda obj: f"{obj.username}@example.com")
-    first_name = factory.Faker('first_name')
-    last_name = factory.Faker('last_name')
+from healthy_herron.users.tests.factories import UserFactory
 
 
 class FastFactory(factory.django.DjangoModelFactory):
     """Factory for creating Fast instances for testing."""
-    
+
     class Meta:
         model = Fast
-    
+
     user = factory.SubFactory(UserFactory)
     start_time = factory.LazyFunction(timezone.now)
     end_time = None  # Default to active fast
     emotional_status = None
-    comments = factory.Faker('text', max_nb_chars=128)
-    
+    comments = factory.Faker("text", max_nb_chars=128)
+
     @factory.post_generation
     def completed(obj, create, extracted, **kwargs):
         """Create a completed fast if completed=True is passed."""
         if extracted:
             obj.end_time = obj.start_time + timezone.timedelta(hours=16)
-            obj.emotional_status = factory.Faker('random_element', 
-                                               elements=['energized', 'satisfied', 'challenging', 'difficult']).generate({})
+            obj.emotional_status = factory.Faker("random_element",
+                                               elements=["energized", "satisfied", "challenging", "difficult"]).generate({})
             if create:
                 obj.save()
 
@@ -53,6 +38,6 @@ class ActiveFastFactory(FastFactory):
 class CompletedFastFactory(FastFactory):
     """Factory for creating completed fasts."""
     end_time = factory.LazyAttribute(lambda obj: obj.start_time + timezone.timedelta(hours=16))
-    emotional_status = factory.Faker('random_element', 
-                                   elements=['energized', 'satisfied', 'challenging', 'difficult'])
-    comments = factory.Faker('text', max_nb_chars=100)
+    emotional_status = factory.Faker("random_element",
+                                   elements=["energized", "satisfied", "challenging", "difficult"])
+    comments = factory.Faker("text", max_nb_chars=100)
